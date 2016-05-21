0d59aa6f8888caeba1aaa3d02998104f76a72e6c
perdeepOsingh <perdeep@gmail.com>
2016-05-21 14:07:27 +1000

    - Added test to made 100% code coverage. - Added code coverage support - Added PHP mess detector - PHPCS fixes - Changing travis configuration - Added readme.md with build status - Removed test support for < 5.5 - Fixing broken .travis.yaml - Added simple class - Added basic test - Added travis configurations - Added composer details

--- tests/TestMathLibrary.php
+++ tests/TestMathLibrary.php
@@ -19,4 +19,20 @@ class TestMathLibrary extends PHPUnit_Framework_TestCase
         $additionResult = MathLibrary::addition($numbersArray);
         $this->assertNotEquals(20, $additionResult);
     }
+
+    public function testSubtractionPass()
+    {
+        $number1 = 10;
+        $number2 = 50;
+        $subtractionResult = MathLibrary::subtraction($number1, $number2);
+        $this->assertEquals(-40, $subtractionResult);
+    }
+
+    public function testSubtractionFail()
+    {
+        $number1 = 10;
+        $number2 = 50;
+        $subtractionResult = MathLibrary::subtraction($number1, $number2);
+        $this->assertNotEquals(40, $subtractionResult);
+    }
 }
