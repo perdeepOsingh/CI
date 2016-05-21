8f38881eedd1a266ae7a9de1b1a046bf37c7ea0a
perdeepOsingh <perdeep@gmail.com>
2016-05-21 13:40:34 +1000

    - Added code coverage support - Added PHP mess detector - PHPCS fixes - Changing travis configuration - Added readme.md with build status - Removed test support for < 5.5 - Fixing broken .travis.yaml - Added simple class - Added basic test - Added travis configurations - Added composer details

--- app/MathLibrary.php
+++ app/MathLibrary.php
@@ -1,18 +1,18 @@
 <?php
     namespace app;
 
-    class MathLibrary
+class MathLibrary
+{
+    public static function addition(array $numbers)
     {
-        public static function addition(array $numbers)
-        {
-            $result = array_sum($numbers);
+        $result = array_sum($numbers);
 
-            return $result;
-        }
-
-        public static function subtraction($number1, $number2)
-        {
-            $result = $number1-$number2;
-            return $result;
-        }
+        return $result;
     }
+
+    public static function subtraction($number1, $number2)
+    {
+        $result = $number1-$number2;
+        return $result;
+    }
+}
